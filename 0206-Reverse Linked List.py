e1e7b9687396ddba2e9e8859c37986684435024b
Dev Mehta <mehtadev01@gmail.com>
2025-05-15 19:38:27 -0500

    Solved new questions

--- 0206-Reverse Linked List.py
+++ 0206-Reverse Linked List.py
@@ -6,10 +6,9 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
-        pointer1 = head
-        while pointer1 != None:
-            pointer2 = pointer1.next
-            pointer1.next = prev
-            prev = pointer1
-            pointer1 = pointer2
+        while head != None:
+            pointer2 = head.next
+            head.next = prev
+            prev = head
+            head = pointer2
         return prev
