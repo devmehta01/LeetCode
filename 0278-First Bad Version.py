e79f1afdd23506b8cca4d667e72c256112c75230
Dev Mehta <mehtadev01@gmail.com>
2025-05-19 16:51:21 -0500

    Solved new questions

--- 0278-First Bad Version.py
+++ 0278-First Bad Version.py
@@ -5,13 +5,11 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         start = 1
         end = n
-        first = 0
 
         while start <= end:
             mid = (start + end) // 2
             if isBadVersion(mid):
-                first = mid
                 end = mid - 1
             else:
                 start = mid + 1
-        return first
+        return start
