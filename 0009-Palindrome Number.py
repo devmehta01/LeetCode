e1c4031d779a9ff0d275e3f326739794d500bbc0
Dev Mehta <mehtadev01@gmail.com>
2025-05-21 12:16:02 -0500

    Solved new questions

--- 0009-Palindrome Number.py
+++ 0009-Palindrome Number.py
@@ -5,4 +5,16 @@ class Solution(object):
         :rtype: bool
         """
         return str(x) == str(x)[::-1]
-        
+
+
+######### ANOTHER METHOD
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x%10 == 0 and x!=0):
+            return False
+        rev = 0
+
+        while rev < x:
+            rev = rev*10 + x%10
+            x //= 10
+        return rev == x or x == rev//10
